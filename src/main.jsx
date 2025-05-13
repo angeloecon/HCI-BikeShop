56b1d01d070ea7d0d43f91d3e8c95a7dc8e7638c
angelo <econ7341@gmail.com>
2025-05-14 00:48:31 +0800

    revert

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
     <App />
     </BrowserRouter>
   </StrictMode>,
